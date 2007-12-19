53db8835e6e275f54a3996cfa740a43328854553
Horst Tritremmel <peter_weber69@gmx.at>
2007-12-19 19:49:25 +0000

    * search in treeview1 (mountpoints of other partitions) disabled

--- src/interface.c
+++ src/interface.c
@@ -404,6 +404,7 @@ create_window_main (void)
   gtk_widget_show (treeview1);
   gtk_container_add (GTK_CONTAINER (scrolledwindow1), treeview1);
   gtk_widget_set_size_request (treeview1, 248, 136);
+  gtk_tree_view_set_enable_search (GTK_TREE_VIEW (treeview1), FALSE);
   gtk_tree_view_set_hover_selection (GTK_TREE_VIEW (treeview1), TRUE);
   gtk_tree_view_set_hover_expand (GTK_TREE_VIEW (treeview1), TRUE);
 
